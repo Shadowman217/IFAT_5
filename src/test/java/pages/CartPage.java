eaf1868e4836ba1232b0c4745b1515c132905c18
Shadowman217 <unknownb57@gmail.com>
2025-12-19 15:21:19 +0400

    Day3. Add allure + fix mistakes

--- src/test/java/pages/CartPage.java
+++ src/test/java/pages/CartPage.java
@@ -8,12 +8,13 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class CartPage extends BasePage {
+    private static final By PRODUCT_NAME_LOCATOR = By.cssSelector(".inventory_item_name");
     public CartPage(WebDriver driver) {
         super(driver);
     }
 
     public ArrayList<String> getProductsNames() {
-        List<WebElement> allProductsNames = driver.findElements(By.cssSelector(".inventory_item_name"));
+        List<WebElement> allProductsNames = driver.findElements(PRODUCT_NAME_LOCATOR);
         ArrayList<String> names = new ArrayList<>();
         for (WebElement product : allProductsNames) {
             names.add(product.getText());
